afcd70e30ad223c6b0f764f643192f2f88179c25
AkashBhave <akash_bhave@hotmail.com>
2016-01-24 18:41:13 -0500

    java-calculator 2.5
    Limits `TextField` entry to only acceptable values (numbers and commas).
    Adds `Label` above `TextField` that shows the whole string of buttons
    typed. Cleaner interface, color added. Now able to successfully
    implement Stylesheets into `javafx3.java`. All buttons are now
    functional, and typing is smooth (auto focuses cursor). No major changes
    to `calc6.java`.

---
+++ calc6.java
@@ -0,0 +1,224 @@
+package Calculator;
+
+import java.util.Arrays;
+import java.lang.Math;
+
+
+/**
+ * Created by Akash Bhave on 1/21/2016.
+ */
+public class calc6 {
+    public static void main(String[] args) {
+
+        /*System.out.println(basFun.add(34.0192312, -45.23453));
+        System.out.println(basFun.subt(34.0192312, -45.23453));
+        System.out.println(basFun.mult(34.0192312, -45.23453));
+        System.out.println(basFun.divd(34.0192312, -45.23453));
+        System.out.println(basFun.mod(34.0192312, -45.23453));
+        System.out.println(basFun.abs(-2342.17645));
+        System.out.println(basFun.exp(4, 4));
+        System.out.println(statFun.mad(13,14,15,67,24,12,3));
+        System.out.println(statFun.standev(13, 14, 14, 14, 16, 24, 28, 28));
+        System.out.println(statFun.varnce(13, 14, 14, 14, 16, 24, 28, 28));
+        System.out.println(statFun.median(1,9,4,5,7,7,21,29,12,25));
+        System.out.println(statFun.range(1,9,4,5,7,7,21,29,12,25));*/
+        System.out.println(statFun.mode(1, 2, 10, 9, 4, 4, 4, 3, 3, 3, 6));
+        javafx3.main(args);
+    }
+}
+class basFun {
+    // Adds two numbers
+    public static double add(double val1, double val2) {
+        double ans = val1 + val2;
+        return ans;
+    }
+    // Subtracts two numbers
+    public static double subt(double val1,double val2) {
+        double ans = val1 - val2;
+        return ans;
+    }
+    // Multiplies two numbers
+    public static double mult(double val1,double val2) {
+        double ans = val1 * val2;
+        return ans;
+    }
+    // Divides two numbers
+    public static double divd(double val1,double val2) {
+        double ans = val1 / val2;
+        return ans;
+    }
+    // Finds the remainder of two numbers
+    public static double mod(double val1,double val2) {
+        double ans = val1 % val2;
+        return ans;
+    }
+    // Finds the absolute value of a number
+    public static double abs(double val) {
+        double ans = Math.abs(val);
+        return ans;
+    }
+    // Calculates one number to the nth power
+    public static double exp(double base, double power) {
+        double ans = Math.pow(base, power);
+        return ans;
+    }
+    // Squares the given number
+    public static double sqre(double val) {
+        double ans = val * val;
+        return ans;
+    }
+    // Finds the square root of a given number
+    public static double sqrt(double val) {
+        double ans = Math.sqrt(val);
+        return ans;
+    }
+}
+class advFun {
+    // Finds the sine of a number
+    public static double sin(double val) {
+        double ans = Math.sin(val);
+        return ans;
+    }// Finds the cosine of a number
+    public static double cos(double val) {
+        double ans = Math.cos(val);
+        return ans;
+    }// Finds the tangent of a number
+    public static double tan(double val) {
+        double ans = Math.tan(val);
+        return ans;
+    }
+    // Finds the cosecant(inverse of sine) of a number
+    public static double csc(double val) {
+        double ans = 1 / (Math.sin(val));
+        return ans;
+    }// Finds the secant(inverse of cosine) of a number
+    public static double sec(double val) {
+        double ans = 1 / (Math.cos(val));
+        return ans;
+    }// Finds the cotangent(inverse of tangent) of a number
+    public static double cot(double val) {
+        double ans = 1 / (Math.tan(val));
+        return ans;
+    }
+    // Returns the natural logarithm of a number
+    public static double natlog(double val) {
+        double ans = Math.log(val);
+        return ans;
+    }
+    // Returns the logarithm of a number
+    public static double log10(double val) {
+        double ans = Math.log10(val);
+        return ans;
+    }
+
+}
+class statFun {
+    // Returns the mean of a data set
+    public static double mean(double ... vals) {
+        double summation = 0;
+        int valCount = 0;
+        for(double val : vals) {
+            summation += val;
+            valCount ++;
+        }
+        double mean = summation / valCount;
+        return mean;
+    }// Returns the median of a data set
+    public static double median(double ... vals) {
+        Arrays.sort(vals);
+        double median;
+        if (vals.length % 2 == 0) {
+            // Finds the two numbers in the middle and finds the average
+            median = ((double) vals[vals.length / 2] + (double) vals[vals.length / 2 - 1]) / 2;
+            return median;
+        } else {
+            median = (double) vals[vals.length / 2];
+        return median;
+        }
+
+    }// Returns the GREATEST mode of the data set
+    public static double mode(double ... vals) {
+        double maxValue = -1;
+        int maxCount = 0;
+        for(int i = 0; i < vals.length; i++) {
+            // count number of times vals[i] is in array
+            int count = 0;
+            for(int j = 0; j < vals.length; j++) {
+                if(vals[j] == vals[i]) {
+                    count++;
+                }
+            }
+
+            // remember the highest count we have seen
+            if(count > maxCount) {
+                maxValue = vals[i];
+                maxCount = count;
+            }
+        }
+        return maxValue;
+    }
+    // Returns the range of the data set
+    public static double range(double ... vals) {
+        Arrays.sort(vals);
+        int lastVal = vals.length - 1;
+        double range = vals[lastVal] - vals[0];
+        return range;
+    }
+    // Finds the Mean Absolute Deviation of a data set
+    public static double mad(double ... vals) {
+        // sums all the numbers
+        double summation = 0;
+        int valCount = 0;
+        for(double val : vals) {
+            summation += val;
+            valCount ++;
+        }
+        double mean = summation / valCount;
+        double madSum = 0;
+        // Sums the absolute value of a number minus the mean
+        for(double aVal : vals) {
+            madSum += Math.abs(aVal - mean);
+        }
+        double mad = madSum / valCount;
+        return mad;
+    }
+    // Finds the Standard Deviation of a data set
+    public static double standev(double ... vals) {
+        double summation = 0;
+        int valCount = 0;
+        for(double val : vals) {
+            summation += val;
+            valCount ++;
+        }
+        double mean = summation / valCount;
+        double sdSum = 0;
+        // Sums the value a number minus the mean squared
+        for(double aVal : vals) {
+            sdSum += Math.pow((aVal - mean), 2);
+        }
+        // SD without taking the square root
+        double noSqrRoot = sdSum / valCount;
+        // Takes the square root
+        double standDev = Math.sqrt(noSqrRoot);
+        return standDev;
+    }
+    // Finds the Variance of a data set
+    public static double varnce(double ... vals) {
+        double summation = 0;
+        int valCount = 0;
+        for(double val : vals) {
+            summation += val;
+            valCount ++;
+        }
+        double mean = summation / valCount;
+        double varSum = 0;
+        // Sums the value a number minus the mean squared
+        for(double aVal : vals) {
+            varSum += Math.pow((aVal - mean), 2);
+        }
+        // SD without taking the square root
+        double variance = varSum / valCount;
+        return variance;
+
+    }
+}
---
+++ javafx3.java
@@ -0,0 +1,205 @@
+package Calculator;
+
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+/**
+ * Created by Akash Bhave on 1/21/2016.
+ */
+public class javafx3 extends Application {
+    String TYPE = "";
+    String VAL1 = "";
+    String VAL2 = "";
+    String currLabelVal = "";
+    TextField mainField = new TextField();
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        basFun basic = new basFun();
+
+        Label cmplt = new Label("");
+        cmplt.setPadding(new Insets(-5, 0, 5, 0));
+        cmplt.setFont(Font.font("Open Sans", 10));
+
+        VBox topField = new VBox();
+        topField.setPadding(new Insets(10, 10, 0, 10)); //Insets: top/rigsdfasdfht/bottom/left
+        topField.setAlignment(Pos.TOP_RIGHT);
+
+        mainField.setPrefHeight(50);
+        mainField.setFont(Font.font("Roboto", 25));
+        mainField.setAlignment(Pos.CENTER_RIGHT);
+        mainField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                String[] allChars = mainField.getText().split("");
+                //Ensures only numbers and
+                for(String i : allChars) {
+                    if ("1234567890.,".contains(i)) {
+                        cmplt.setText("");
+                        String lclCmpltText = currLabelVal;
+                        cmplt.setText(lclCmpltText + newValue);
+                    } else {
+                        cmplt.setText(oldValue);
+                        mainField.setText(oldValue);
+                    }
+                }
+            }
+        });
+
+        GridPane layout = new GridPane();
+        layout.setPadding(new Insets(0,0,0,10));
+        layout.setVgap(10);
+        layout.setHgap(10);
+        layout.setAlignment(Pos.TOP_RIGHT);
+
+        // ***** MAIN BUTTON DECLARATIONS *****
+        Button bAdd = new Button();
+        bAdd.setText("+");
+        bAdd.setFont(Font.font("Open Sans", FontWeight.BOLD, 20));
+        bAdd.setPrefSize(50,50);
+        GridPane.setConstraints(bAdd, 4, 1);
+        bAdd.setOnAction(e -> {
+            setup("bAdd");
+            String cmpltText = cmplt.getText() + " + ";
+            mainField.clear();
+            cmplt.setText(cmpltText);
+            currLabelVal = cmpltText;
+        });
+
+        Button bSubt = new Button();
+        bSubt.setText("-");
+        bSubt.setFont(Font.font("Open Sans", FontWeight.BOLD, 20));
+        bSubt.setPrefSize(50,50);
+        GridPane.setConstraints(bSubt, 4, 2);
+        bSubt.setOnAction(e -> {
+            setup("bSubt");
+            String cmpltText = cmplt.getText() + " - ";
+            mainField.clear();
+            cmplt.setText(cmpltText);
+            currLabelVal = cmpltText;
+        });
+
+        Button bMult = new Button();
+        char multiplicationSign = '\u00D7';
+        bMult.setText(Character.toString(multiplicationSign));
+        bMult.setFont(Font.font("Open Sans", FontWeight.BOLD, 20));
+        bMult.setPrefSize(50,50);
+        GridPane.setConstraints(bMult, 4, 3);
+        bMult.setOnAction(e -> {
+            setup("bMult");
+            String cmpltText = cmplt.getText() + " " + Character.toString(multiplicationSign) + " ";
+            mainField.clear();
+            cmplt.setText(cmpltText);
+            currLabelVal = cmpltText;
+        });
+
+        Button bDivd = new Button();
+        char divisionSign = '\u00F7';
+        bDivd.setText(Character.toString(divisionSign));
+        bDivd.setFont(Font.font("Open Sans", FontWeight.BOLD, 20));
+        bDivd.setPrefSize(50,50);
+        GridPane.setConstraints(bDivd, 4, 4);
+        bDivd.setOnAction(e -> {
+            setup("bDivd");
+            String cmpltText = cmplt.getText() + " " + Character.toString(divisionSign) + " ";
+            mainField.clear();
+            cmplt.setText(cmpltText);
+            currLabelVal = cmpltText;
+        });
+
+        Button bMod = new Button();
+        bMod.setText("MOD");
+        bMod.setFont(Font.font("Open Sans", 10));
+        bMod.setPrefSize(50,30);
+        GridPane.setConstraints(bMod, 4, 5);
+        bMod.setOnAction(e -> {
+            setup("bMod");
+            String cmpltText = cmplt.getText() + " % ";
+            mainField.clear();
+            cmplt.setText(cmpltText);
+            currLabelVal = cmpltText;
+        });
+
+
+        Button bEQUALS = new Button();
+        bEQUALS.setStyle("-fx-base: #009d00");
+        bEQUALS.setText("=");
+        bEQUALS.setFont(Font.font(("Open Sans"), FontWeight.BOLD, 20));
+        bEQUALS.setPrefSize(50, 140);
+        GridPane.setConstraints(bEQUALS, 4, 6);
+        bEQUALS.setOnAction(e -> {
+            currLabelVal = "";
+
+            VAL2 = mainField.getText();
+            double val1 = Double.parseDouble(VAL1);
+            double val2 = Double.parseDouble(VAL2);
+            if (TYPE.equals("bAdd")) {
+                String result = Double.toString(basFun.add(val1, val2));
+                mainField.setText(result);
+            }
+            else if(TYPE.equals("bSubt")) {
+                String result = Double.toString(basFun.subt(val1, val2));
+                mainField.setText(result);
+            }
+            else if(TYPE.equals("bMult")) {
+                String result = Double.toString(basFun.mult(val1, val2));
+                mainField.setText(result);
+            }
+            else if(TYPE.equals("bDivd")) {
+                String result = Double.toString(basFun.divd(val1, val2));
+                mainField.setText(result);
+            }
+            else if(TYPE.equals("bMod")) {
+                String result = Double.toString(basFun.mod(val1, val2));
+                mainField.setText(result);
+            }
+        });
+
+        layout.getChildren().addAll(mainField, bAdd, bSubt, bMult, bDivd, bMod, bEQUALS);
+        topField.getChildren().addAll(cmplt, mainField, layout);
+
+        Scene scene = new Scene(topField, 500, 505);
+        scene.getStylesheets().clear();
+        scene.getStylesheets().add("file:///C:\\CODING\\.idea\\src\\Calculator\\style2.css".replace("\\", "/"));
+
+        primaryStage.setTitle("Calculator");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private boolean isGood(String input) {
+        try {
+            double val = Double.parseDouble(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public void setup(String caller) {
+        String val = mainField.getText();
+        VAL1 = val;
+        TYPE = caller;
+        mainField.requestFocus();
+    }
+
+}
